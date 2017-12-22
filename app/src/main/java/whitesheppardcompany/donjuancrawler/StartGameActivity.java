89d83af412beb0b3aed6c940d28f45d7b6314c4a
Thomas Bastien <sawyer54@hotmail.fr>
2017-12-22 11:42:52 +0100

    add ressource before to go

--- app/src/main/java/whitesheppardcompany/donjuancrawler/StartGameActivity.java
+++ app/src/main/java/whitesheppardcompany/donjuancrawler/StartGameActivity.java
@@ -3,6 +3,8 @@ package whitesheppardcompany.donjuancrawler;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import whitesheppardcompany.donjuancrawler.Beans.Player;
 
@@ -11,6 +13,9 @@ public class StartGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_start_game);
         Player player = (Player)getIntent().getSerializableExtra("player");
     }
